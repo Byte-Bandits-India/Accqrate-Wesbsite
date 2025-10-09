80e605659e59195ae6af88e3226278fa7618dc98
Kousik S <kousik.s@bytebandits.in>
2025-10-09 11:32:22 +0530

    Update section4.tsx

--- src/app/business/books/sections/section4.tsx
+++ src/app/business/books/sections/section4.tsx
@@ -21,7 +21,7 @@ export default function AutomationSection(): JSX.Element {
     {
       label: "AI Bank Reconciliation",
       content:
-        "Imports, matches, and reconciles your statements—up to 10x faster.",
+        "Imports, matches, and reconciles your statements up to 10x faster.",
     },
     {
       label: "Auto-Invoice Conversion",
@@ -162,31 +162,29 @@ export default function AutomationSection(): JSX.Element {
 
   return (
     <>
-      <div className="bg-white px-6 md:px-[32px] max-w-[1280px] mx-auto">
+   <div className="bg-white px-6 md:px-[32px] max-w-[1280px] mx-auto mt-6 md:mt-8">
         {/* ================= AI-Driven Automation ================= */}
         <FadeUp className="border-[#E3E3E3] border-[22px] font-inter md:h-auto lg:h-auto xl:h-[615px] py-6 px-4 rounded-xl md:rounded-2xl mx-auto mb-6 md:mb-[56px]
   md:flex md:items-center">
 
           {/* Left Side */}
           <div className="flex-1 md:max-w-[400px] lg:max-w-[520px]">
-            <div>
-              <h2 className="text-[#1976D2] text-[20px] font-medium md:text-[24px] lg:text-[30px] pb-4 leading-tight tracking-para">
-                AI-Driven Automation
-              </h2>
-              <p className="pb-6 md:pb-[32px] text-[14px] md:text-[16px] lg:text-[18px] leading-tight tracking-para text-[#333333]">
-                Let AI handle the repetitive free your team for what matters.
-              </p>
-            </div>
+            <h2 className="text-[#1976D2] text-fluid-body pb-6 leading-snug">
+              AI-Driven Automation
+            </h2>
+            <p className="pb-6 md:pb-[32px] text-[14px] md:text-[18px] lg:text-[16px] md:leading-relaxed md:tracking-wide">
+              Let AI handle the repetitive free your team for what matters.
+            </p>
 
             {/* Accordion */}
             <div className="">
               <Accordion type="single" collapsible className="w-full max-w-[400px]">
                 {dropdowns.map((item, index) => (
                   <AccordionItem key={index} value={`rec-${index}`}>
-                    <AccordionTrigger className="text-[16px] md:text-[16px] lg:text-[18px] font-normal">
+                    <AccordionTrigger className="text-fluid-small md:text-[16px] font-normal">
                       {item.label}
                     </AccordionTrigger>
-                    <AccordionContent className="text-[14px] md:text-[16px] lg:text-[18px]">
+                    <AccordionContent className="text-fluid-small md:text-[16px]">
                       {item.content}
                     </AccordionContent>
                   </AccordionItem>
@@ -203,7 +201,7 @@ export default function AutomationSection(): JSX.Element {
               muted
               loop
             >
-              <source src="/videos/business/books/Ai-driven_automation.mp4" type="video/mp4" />
+              <source src="videos/Ai-driven_automation.mp4" type="video/mp4" />
             </video>
           </div>
         </FadeUp>
@@ -212,19 +210,17 @@ export default function AutomationSection(): JSX.Element {
         {/* Bottom Section */}
         <div className="my-12 mx-auto font-inter md:max-w-5xl ">
           {/* Heading */}
-          <FadeUp className="text-center mb-4 tracking-heading md:flex  md:justify-center leading-tight">
-            <h2 className="text-[26px] md:text-[36px] font-medium text-[#333333]">
-              Support, Training, &
-            </h2>
-            <h2 className="text-[26px] md:text-[36px] md:ml-2 font-medium text-[#1976D2]  ">
-              Onboarding
+          <FadeUp className="text-center mb-6 md:mb-4 md:flex md:justify-center">
+            <h2 className="text-fluid-h2 font-semibold">
+              Support, Training, & <br className="md:hidden" />
+              <span className="font-semibold text-[#1976D2]"> Onboarding</span>
             </h2>
           </FadeUp>
 
           <FadeUp>
-            <p className="text-[14px]  text-[#737373] tracking-para md:text-[18px] lg:text-[20px] mb-6 md:mb-[32px] text-center text-base leading-relaxed">
+            <p className="text-fluid-small lg:text-[16px] mb-6 md:mb-[32px] text-center leading-relaxed">
               Expert onboarding for ZATCA compliance software get your team ready for
-              e-invoicing in KSA, fast.
+              e-invoicing in KSA fast.
             </p>
           </FadeUp>
 
@@ -247,15 +243,15 @@ export default function AutomationSection(): JSX.Element {
                   }}
                 >
                   {/* Mobile Video */}
-                  <div className="h-[185px] m-6 rounded-lg shadow-md md:hidden border border-grey-400">
+                  <div className="h-[185px] m-4 rounded-lg shadow-md md:hidden border border-grey-400">
                     <video
                       autoPlay
                       loop
                       muted
                       playsInline
-                      className="w-[270px] h-[130px] mx-auto mt-8 object-cover rounded-t-lg"
+                      className="w-full h-[125px] mt-8 object-cover rounded-t-lg"
                     >
-                      <source src="/videos/business/books/on-demand-training.mp4" type="video/mp4" />
+                      <source src="/videos/on-demand-training.mp4" type="video/mp4" />
                     </video>
                   </div>
 
@@ -267,19 +263,19 @@ export default function AutomationSection(): JSX.Element {
                     playsInline
                     className="hidden md:block w-full h-[170px] object-cover rounded-t-lg"
                   >
-                    <source src="/videos/business/books/on-demand-training.mp4" type="video/mp4" />
+                    <source src="/videos/on-demand-training.mp4" type="video/mp4" />
                   </video>
 
                   {/* Content */}
-                  <div className="px-4 md:p-4 flex-1 ">
-                    <h3 className="font-semibold tracking-heading text-[20px] md:text-[18px] lg:text-fluid-body text-center">
+                  <div className="p-4 flex-1">
+                    <h3 className="font-semibold text-[20px] md:text-[18px] lg:text-[20px] text-center">
                       On Demand Training
                     </h3>
-                    <p className="text-[#737373] tracking-para leading-tight text-fluid-small mt-2  text-center">
+                    <p className="text-[#737373] text-fluid-small lg:text-[16px] mt-2  text-center">
                       Learn Accqrate Books in under 60 minutes video modules for every type.
                     </p>
-                    <p className="absolute bottom-2 left-2 text-[16px] md:text-[16px] text-[#1976D2] font-bold leading-tight inline-block">
-                      Book An <br className="hidden lg:block" /> Appointment
+                    <p className="absolute bottom-2 left-4 text-[16px] text-[#1976D2] font-bold leading-tight inline-block">
+                      Book An <br className="hidden md:block" /> Appointment
                     </p>
                   </div>
                 </div>
@@ -316,7 +312,7 @@ export default function AutomationSection(): JSX.Element {
                       "polygon(0% 0%, 100% 0%, 100% 90%, 66% 90%, 52% 100%, 100% 100%, 0% 100%)",
                   }}
                 >
-                  <div className="h-[185px] m-6 md:m-4 rounded-lg shadow-md md:hidden border border-grey-400">
+                  <div className="h-[185px] m-4 rounded-lg shadow-md md:hidden border border-grey-400">
                     <video
                       autoPlay
                       loop
@@ -324,21 +320,21 @@ export default function AutomationSection(): JSX.Element {
                       playsInline
                       className="w-full h-[125px] mt-6 md:h-[200px] object-cover rounded-t-lg"
                     >
-                      <source src="/videos/business/books/local-support.mp4" type="video/mp4" />
+                      <source src="/videos/local-support.mp4" type="video/mp4" />
                     </video>
                   </div>
                   <video autoPlay loop muted playsInline className="hidden md:block w-full h-[170px] object-cover rounded-t-lg">
-                    <source src="/videos/business/books/local-support.mp4" type="video/mp4" />
+                    <source src="/videos/local-support.mp4" type="video/mp4" />
                   </video>
 
-                  <div className="px-4 md:p-4 flex-1">
-                    <h3 className="font-semibold tracking-para text-[20px] md:text-fluid-body text-center">
+                  <div className="p-4 flex-1">
+                    <h3 className="font-semibold text-[20px] md:text-[18px] lg:text-[20px] text-center">
                       24/7 Local Support
                     </h3>
-                    <p className="text-[#737373] tracking-para px-2 leading-tight text-fluid-small mt-2 text-center">
+                    <p className="text-[#737373] text-fluid-small lg:text-[16px] mt-2 text-center">
                       In English & Arabic phone, WhatsApp, email, or chat.
                     </p>
-                    <p className="absolute bottom-2 left-1 text-[#1976D2] text-[16px] md:text-[15px] text-left font-bold inline-block">
+                    <p className="absolute bottom-2 left-1 text-[#1976D2] text-[16px] text-left font-bold inline-block">
                       +971505515388
                     </p>
                   </div>
@@ -382,31 +378,31 @@ export default function AutomationSection(): JSX.Element {
                     playsInline
                     className="w-full h-[200px] md:h-[170px] object-cover rounded-t-lg hidden md:block"
                   >
-                    <source src="/videos/business/books/knowledge-center.mp4" type="video/mp4" />
+                    <source src="/videos/knowledge-center.mp4" type="video/mp4" />
                   </video>
 
                   {/* Mobile Video */}
-                  <div className="h-[185px] m-6 rounded-lg shadow-md md:hidden border border-grey-400">
+                  <div className="h-[185px] m-4 rounded-lg shadow-md md:hidden border border-grey-400">
                     <video
                       autoPlay
                       loop
                       muted
                       playsInline
-                      className="w-[270px] h-[125px] mt-8 object-cover rounded-t-lg"
+                      className="w-full h-[125px] mt-8 object-cover rounded-t-lg"
                     >
-                      <source src="/videos/business/books/knowledge-center.mp4" type="video/mp4" />
+                      <source src="/videos/business/books/on-demand-training.mp4" type="video/mp4" />
                     </video>
                   </div>
 
                   {/* Text content */}
-                  <div className="md:p-4 flex-1 relative">
-                    <h3 className="font-semibold text-[20px] tracking-para md:text-fluid-body text-center">
+                  <div className="p-4 flex-1 relative">
+                    <h3 className="font-semibold text-[20px] md:text-[18px] lg:text-[20px] text-center">
                       Knowledge Center
                     </h3>
-                    <p className="text-[#737373] text-fluid-small tracking-para text-center mt-2">
+                    <p className="text-[#737373] text-fluid-small lg:text-[16px] mt-2 text-center">
                       Step-by-step Guides for every process.
                     </p>
-                    <p className="absolute bottom-2 left-4 text-[#1976D2] text-[16px] md:text-[16px] font-bold leading-tight inline-block">
+                    <p className="absolute bottom-2 left-4 text-[#1976D2] text-[16px] font-bold leading-tight inline-block">
                       Knowledge <br className="hidden md:block" /> Center
                     </p>
                   </div>
@@ -432,11 +428,11 @@ export default function AutomationSection(): JSX.Element {
       {/* Why Choose Section */}
       <div className="my-12 md:my-[56px] lg:my-10 bg-[#F2F2F2] w-full py-6" >
         <div className="max-w-[1280px] mx-auto px-6 md:px-[32px] font-inter">
-          <FadeUp className="text-[26px] sm:text-[28px] md:text-[32px] lg:text-[38px] text-[#333333] font-semibold mb-6 md:mb-[32px] text-center leading-snug">
+          <FadeUp className="text-fluid-h2 font-medium mb-6 md:mb-[32px] text-center leading-snug">
             Why Choose Accqrate <span className="text-[#1976D2]"><br />Books</span>?
           </FadeUp>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 lg:gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 lg:gap-12">
             {/* Image */}
             <FadeUp className="flex  items-center justify-center">
               <img
@@ -448,7 +444,7 @@ export default function AutomationSection(): JSX.Element {
 
             {/* Text List */}
             <FadeUp>
-              <ul className="text-left list-disc list-inside text-[14px] leading-6 sm:text-[15px] md:text-[16px] lg:text-[18px] space-y-4 md:space-y-6">
+              <ul className="text-left list-disc list-inside text-fluid-small lg:text-[16px] leading-6 space-y-4 md:space-y-6">
                 <li>Stay Ahead of ZATCA Compliance by Design: Never worry about last-minute regulations or audits.</li>
                 <li>Modern Cloud Accounting: Access your accounting software anywhere. Simplify financial management for every team member.</li>
                 <li>Device and Vendor Freedom: Use any device, integrate any bank, no hardware lock-in.</li>
