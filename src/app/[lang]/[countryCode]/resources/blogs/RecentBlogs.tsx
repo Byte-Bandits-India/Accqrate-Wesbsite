baabc57ce4471efb5432d285b445bcd465bf0fba
Kousik S <kousik.s@bytebandits.in>
2025-10-28 19:25:02 +0530

    Update RecentBlogs.tsx

--- src/app/[lang]/[countryCode]/resources/blogs/RecentBlogs.tsx
+++ src/app/[lang]/[countryCode]/resources/blogs/RecentBlogs.tsx
@@ -2,45 +2,7 @@
 
 import Image from "next/image";
 import FadeUp from "@/components/ui/FadeUp";
-
-const blogPosts = [
-  {
-    id: 1,
-    title: "Ensuring Compliance and Efficiency: A Guide to ZATCA's FATOORA ...",
-    image: "/images/resources/blog-1.png",
-    desc: "In the rapidly evolving landscape of e-invoicing in Saudi Arabia, ensuring accuracy of regulatory requirements is crucial for businesses...",
-  },
-  {
-    id: 2,
-    title: "E-Invoicing in Oman: A Comprehensive Guide to Preparation ...",
-    image: "/images/resources/blog-2.png",
-    desc: "The Sultanate of Oman Tax Authority is poised to usher in electronic invoicing (e-Invoicing) in 2023. Initially planned to be launched...",
-  },
-  {
-    id: 3,
-    title: "Fines and Financial Penalties exemption for its Taxpayers by ...",
-    image: "/images/resources/blog-3.png",
-    desc: "Fines and Financial Penalties exemption for its Taxpayers by ZATCA, Saudi Arabia: The Zakat, Tax and Customs Authority (ZATCA) has ...",
-  },
-  {
-    id: 4,
-    title: "Ensuring Compliance and Efficiency: A Guide to ZATCA's FATOORA ...",
-    image: "/images/resources/blog-1.png",
-    desc: "In the rapidly evolving landscape of e-invoicing in Saudi Arabia, ensuring accuracy of regulatory requirements is crucial for businesses...",
-  },
-  {
-    id: 5,
-    title: "E-Invoicing in Oman: A Comprehensive Guide to Preparation ...",
-    image: "/images/resources/blog-2.png",
-    desc: "The Sultanate of Oman Tax Authority is poised to usher in electronic invoicing (e-Invoicing) in 2023. Initially planned to be launched...",
-  },
-  {
-    id: 6,
-    title: "Fines and Financial Penalties exemption for its Taxpayers by ...",
-    image: "/images/resources/blog-3.png",
-    desc: "Fines and Financial Penalties exemption for its Taxpayers by ZATCA, Saudi Arabia: The Zakat, Tax and Customs Authority (ZATCA) has ...",
-  },
-];
+import blogPosts from "./data/blogspost";
 
 export default function RecentBlogPosts() {
   return (
@@ -50,13 +12,13 @@ export default function RecentBlogPosts() {
           Recent Blog Posts from Accqrate
         </FadeUp>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-8">
           {blogPosts.map((post) => (
             <div
               key={post.id}
               className="rounded-xl overflow-hidden duration-300 border mb-[50px] md:mb-0 border-gray-100"
             >
-              <FadeUp className="relative w-full rounded-xl h-[188px] lg:h-[250px]">
+              <FadeUp className="relative w-full rounded-xl h-[188px]  xl:h-[250px]">
                 <Image
                   src={post.image}
                   alt={post.title}
@@ -65,11 +27,12 @@ export default function RecentBlogPosts() {
                   sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
                 />
               </FadeUp>
+
               <div className="p-4">
-                <FadeUp className="font-semibold text-fluid-body lg:text-[26px] text-gray-900 line-clamp-2 leading-tight mb-[20px] md:mb-[30px]">
+                <FadeUp className="font-semibold text-fluid-body  lg:h-[70px] lg:mt-10 lg:text-[26px] text-gray-900 line-clamp-2 leading-tight mb-[10px] md:mb-[30px]">
                   {post.title}
                 </FadeUp>
-                <FadeUp className="text-gray-600 text-fluid-small line-clamp-3 leading-relaxed">
+                <FadeUp className="text-gray-600 text-fluid-small line-clamp-3 leading-tight">
                   {post.desc}
                 </FadeUp>
               </div>
