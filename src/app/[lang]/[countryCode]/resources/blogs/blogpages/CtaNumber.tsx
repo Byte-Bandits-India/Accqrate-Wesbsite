57328bcafaab32cc0297a4da9d4456a3c1f0ae63
Kousik S <kousik.s@bytebandits.in>
2025-10-30 18:26:21 +0530

    Update CtaNumber.tsx

--- src/app/[lang]/[countryCode]/resources/blogs/blogpages/CtaNumber.tsx
+++ src/app/[lang]/[countryCode]/resources/blogs/blogpages/CtaNumber.tsx
@@ -2,15 +2,19 @@
 
 import Link from "next/link";
 import T from "@/components/T";
+import { useParams } from "next/navigation";
 
 export default function CtaNumber({ className = "" }) {
+      const params = useParams();
+        const lang = params?.lang as string;
+        const countryCode = params?.countryCode as string;
   return (
     <div className={`pt-3 text-sm sm:text-base ${className}`}>
-      <Link href="/faq" className="text-blue-600 hover:underline">
+      <Link href={`/${lang}/${countryCode}/resources/faq`} className="text-blue-600 hover:underline">
         <T>Got questions</T>
       </Link>{" "}
       or{" "}
-      <Link href="/contact-us" className="text-blue-600 hover:underline">
+      <Link href={`/${lang}/${countryCode}/contact-us`} className="text-blue-600 hover:underline">
         <T>need a demo?</T>
       </Link>{" "}
       <T>Drop us a mail at</T>{" "}
