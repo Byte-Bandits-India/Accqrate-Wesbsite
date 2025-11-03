f97b6192e85d1bc0eb122e5ea97d6f3bb18f9d62
Kousik S <kousik.s@bytebandits.in>
2025-11-03 11:31:08 +0530

    Update CommingSoon.tsx

--- src/components/CommingSoon.tsx
+++ src/components/CommingSoon.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import T from '@/components/T' // optional: for translations
 import React from 'react'
-
+import ZatcaSection from '@/app/[lang]/[countryCode]/resources/faq/sections/ZatcaSection'
 interface ComingSoonProps {
     title?: string
     subtitle?: string
@@ -30,28 +30,17 @@ export default function ComingSoon({
     const targetUrl = `/${lang || 'en'}/${countryCode || 'sa'}${redirectPath}`
 
     return (
+        <>
         <div
-            className={`min-h-[70vh] flex flex-col items-center justify-start bg-[#F8F6FF] text-center px-6 ${className}`}
+            className={`min-h-[60vh] flex flex-col items-center justify-center bg-[#F8F6FF] text-center px-6 ${className}`}
         >
-            <h1 className="text-4xl md:text-6xl font-semibold text-gray-900 mb-10 mt-20 md:mb-28 md:mt-40">
+            <h1 className="text-3xl md:text-[50px] font-semibold text-gray-900 mb-10 mt-20  ">
                 <T>{title}</T>
             </h1>
 
-            <p className="text-lg md:text-xl font-semibold text-gray-800 mb-3">
-                <T>{subtitle}</T>
-            </p>
-
-            <p className="text-sm md:text-base text-gray-600 mb-12">
-                <T>{description}</T>
-            </p>
-
-            <Link
-                href={targetUrl}
-                className="inline-flex items-center text-white font-semibold px-6 py-3 rounded-full bg-gradient-to-r from-[#29266E] to-[#194BED] transition-all duration-300 hover:opacity-90"
-            >
-                <T>{buttonText}</T>
-                <ArrowRight className="ml-2 h-5 w-5" />
-            </Link>
+          
         </div>
+         <ZatcaSection/>
+        </>
     )
 }
