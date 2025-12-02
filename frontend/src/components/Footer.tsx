e96413873342f60d1220c213e65e8f72d5ff2257
abraham-r-bytebandits <abraham.r@bytebandits.in>
2025-12-02 12:44:28 +0530

    correct the error

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { comingSoonRoutes } from '@/lib/comingSoonRoutes'
+import { comingSoonRoutes } from '@/lib/ComingSoonRoutes'
 
 
 // ===================== Custom Hook for Dynamic Routing =====================
