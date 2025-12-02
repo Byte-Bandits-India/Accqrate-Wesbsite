d01349b39813e275270c10d09663b2b9e5ac3b6b
abraham-r-bytebandits <abraham.r@bytebandits.in>
2025-12-02 12:36:32 +0530

    correct the docker

--- src/Util/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  API_URL:
-    process.env.REACT_APP_DOCKER || process.env.NODE_ENV === 'production'
-      ? 'https://prod.accqrate-erp.com/api'
-      : 'http://localhost:9020/api'
-}
--- src/app/[lang]/[countryCode]/business/plan360/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ComingSoon from '@/components/CommingSoon'
-
-export default function Plan360() {
-    return <ComingSoon />
-}
