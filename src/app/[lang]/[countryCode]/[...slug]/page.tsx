c7ae58084b37fdb3dd835796e6a8253626d4e20f
abraham-r-bytebandits <abraham.r@bytebandits.in>
2025-12-02 11:48:38 +0530

    correct the error

--- src/app/[lang]/[countryCode]/[...slug]/page.tsx
+++ src/app/[lang]/[countryCode]/[...slug]/page.tsx
@@ -1,10 +1,70 @@
 'use client'
 
-import React from 'react'
-import { use } from 'react'
+import React, { use } from 'react'
 import ComingSoon from '@/components/CommingSoon'
-import { notFound } from 'next/navigation'
-import { comingSoonRoutes } from '@/lib/ComingSoonRoutes'
+
+const comingSoonRoutes = [
+    // E-invoicing Solution
+    'e-invoicing/accqrate',
+    'e-invoicing/integration',
+    'e-invoicing/sap',
+    'e-invoicing/microsoft',
+    'e-invoicing/oracle',
+    'e-invoicing/custom',
+    'e-invoicing/legacy',
+    'e-invoicing/multi-country',
+
+    // Resources
+    'resources/events',
+    'resources/case-studies',
+    'resources/product-updates',
+    'resources/alternate-product',
+    'resources/knowledge-center',
+
+    // Community
+    'community/affiliates',
+    'community/non-profit',
+    'community/sprouts',
+    'community/become-partner',
+    'community/forum',
+    'community/work-with-partner',
+    'community/find-accountant',
+    'community/marketplace',
+
+    // Data & Safety management
+    'security',
+    'compliance',
+    'cookie-policy',
+    'privacy-policy',
+    'gdpr',
+    'terms',
+    'ipr-complaints',
+
+    'business/services',
+    'business/e-invoicing/accqrate',
+
+    // Left column links
+    'accelera',
+    'testimonials',
+    'careers',
+    'press',
+    'resources/newsletter',
+    'resources/branding-assets',
+
+    // Enterprise links
+    'assessments',
+    'custom-development',
+    'bpa',
+
+    "Enterprise-Solution/Assessments",
+    'Enterprise-Solution/Custom-Developments',
+    'Enterprise-Solution/Business-Process-Automations',
+
+    'contact-sales',
+    'book-demo',
+
+    '/business/services'
+]
 
 export default function CatchAllPage({
     params,
@@ -19,5 +79,5 @@ export default function CatchAllPage({
         return <ComingSoon />
     }
 
-    notFound()
+    return <div>Page not found</div>
 }
